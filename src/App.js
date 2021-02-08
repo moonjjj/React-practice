21bb0780d6d39d53e512aeb7acc7236e6b981b90
moonjjj <mjesun1@naver.com>
2021-02-08 15:42:16 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ class App extends React.Component {
     this.state={
      sub:"babo",
       href:"https://www.naver.com"
+      ,contents:[
+        {id:1, list:'문제선 깃사이트',href:'https://github.com/moonjjj'},
+        {id:2, list:'바닐라 먹킷리스트',href:'https://moonjjj.github.io/js_practice/'},
+        {id:3, list:'문제선 포토폴리오 ver.1.0.0',href:'https://moonjjj.github.io/project-solo/'}
+      ]
     }
   }
   render(){
@@ -24,7 +29,7 @@ class App extends React.Component {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           gazua
         </a>
-        <Nav title1="프롭스를 이용해보았습니다!" sub={this.state.sub} url={this.state.href}></Nav>
+        <Nav data={this.state.contents} title1="프롭스를 이용해보았습니다!" sub={this.state.sub} url={this.state.href}></Nav>
       </header>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,17 @@ class Nav extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      subject:{href:"https://www.youtube.com",
-    name:"youtube"}
+      subject:{href:"https://www.youtube.com",name:"youtube"}
     }
   }  
   
   render(){
+      var lists=[];
+      var data = this.props.data //app에서 주입된 정보를 여기서 사용할 땐 props로 받아야함!
+      var i = 0;
+      for(i=0; i<data.length; i++){
+        lists.push(<li><a href={data[i].href} className={data[i].id}>{data[i].list}</a></li>);
+      }
       return(
         <nav>
           <ul>
@@ -18,6 +23,13 @@ class Nav extends React.Component{
             <li><a href="#">{this.props.title1}</a></li>
             <li><a href="#">js</a></li>
             <li><a href={this.props.url}>{this.props.sub}</a></li>
+            {lists}
+            <li><a href="#" onClick={function(){
+              console.log("온클릭 테스트합니다! react는 onClick이라고 적어줘야하구요");
+              console.log("{}안에 값을 넣어야해요! 그리고 function 인자에 e를 받아서");
+              console.log("preventDefault()메소드를 이용해서 원래 태그의 기본 이벤트를 저지 시켜줍니다");
+              e.preventDefault();
+            }}>온클릭 이벤트에 대한 설명(f12-console 참고)</a></li>
           </ul>
         </nav>
       );
